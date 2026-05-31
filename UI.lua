8a6f88b7f34c8ad7b4871b9b5431f2f9bd343ed4
SilverHub999 <patnarinnm@gmail.com>
2026-05-31 12:00:40 +0700

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1552,7 +1552,7 @@ local ClosureBindings = {
 					Size = UDim2.new(1, -16, 0, 18),
 					Position = UDim2.fromOffset(0, 2),
 					ThemeTag = {
-						TextColor3 = "Text",
+						TextColor3 = "MainText",
 					},
 				}),
 				Section.Container,
